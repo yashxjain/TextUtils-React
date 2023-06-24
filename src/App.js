25315d1beeb02e926691cbe05f4b1b31e20aed38
Yash jain <jyash5332@gmail.com>
2023-06-25 01:58:56 +0530

    build

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
 import Alert from "./components/Alert";
-import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
   const toggleMode = () => {
     if (mode === "light") {
